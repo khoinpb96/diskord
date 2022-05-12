9b9fce4e7d8e4d373178fdb4003e6f5cfee887ad
Kilian Nguyen <khoinpb96@gmail.com>
2022-05-12 16:59:38 +0700

    finish desktop UI for register route

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-import "./index.css";
+import { GlobalStyle } from "./styles";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>
