d6f903342dd6f2695e79df96470640cfbe73a49c
Kilian Nguyen <khoinpb96@gmail.com>
2022-07-16 18:33:20 +0700

    refactor NotfoundPage

--- src/pages/NotfoundPage/NotfoundPage.tsx
+++ src/pages/NotfoundPage/NotfoundPage.tsx
@@ -6,7 +6,7 @@ const NotfoundPage = () => {
     <NotfoundPageWrapper>
       <Header>
         <Link to="/">
-          <img src="/assets/logo.svg" />
+          <Logo src="/assets/logo.svg" />
         </Link>
 
         <LoginButton to="/login">Login</LoginButton>
@@ -15,10 +15,29 @@ const NotfoundPage = () => {
         <Content>
           <h3>WRONG TURN?</h3>
           <p>
-            You look lost, stranger. You know what helps when you're lost? A
-            piping hot bowl of noodles. Take a seat, we're frantically at work
-            here cooking up something good.
+            You look lost, stranger. You know what helps when you’re lost? A
+            piping hot bowl of noodles. Take a seat, we’re frantically at work
+            here cooking up something good. Oh, you need something to read?
+            These might help you:
           </p>
+
+          <OutLinks>
+            <li>
+              <a target="__blank" href="https://discordstatus.com/">
+                Status Page
+              </a>
+            </li>
+            <li>
+              <a target="__blank" href="https://twitter.com/discord">
+                @Discord
+              </a>
+            </li>
+            <li>
+              <a target="__blank" href="https://support.discord.com/hc/en-us">
+                Discord Support
+              </a>
+            </li>
+          </OutLinks>
         </Content>
 
         <Gif src="/assets/404.gif" />
@@ -30,7 +49,6 @@ const NotfoundPage = () => {
 export default NotfoundPage;
 
 const NotfoundPageWrapper = styled.main`
-  background: #5865f2;
   min-height: 100vh;
 `;
 
@@ -42,26 +60,62 @@ const Header = styled.header`
   max-width: 1280px;
   margin: 0 auto;
 `;
+
+const Logo = styled.img`
+  filter: invert(1);
+`;
+
 const Row = styled.section`
-  color: white;
-
   display: flex;
-  gap: 1rem;
-
-  padding: 120px 40px;
+  gap: 40px;
 
   max-width: 1280px;
+  padding: 120px 40px;
   margin: 0 auto;
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+    align-items: center;
+    max-width: 600px;
+  }
 `;
 
 const Content = styled.div`
   h3 {
+    color: #5865f2;
     font-size: 44px;
     font-family: "Black Han Sans", sans-serif;
   }
+
+  p {
+    max-width: 600px;
+  }
+
+  @media (max-width: 1024px) {
+    order: 2;
+  }
 `;
+
+const OutLinks = styled.ul`
+  list-style-type: none;
+  margin-top: 20px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.4rem;
+
+  a {
+    color: #00aff4;
+    text-decoration: none;
+  }
+`;
+
 const Gif = styled.img`
   width: 350px;
+
+  @media (max-width: 1024px) {
+    order: 1;
+  }
 `;
 
 const LoginButton = styled(Link)`
@@ -79,11 +133,16 @@ const LoginButton = styled(Link)`
   transition-duration: 0.2s;
   transition-timing-function: ease-in-out;
 
-  background-color: #ffd400;
-  color: #000;
+  background-color: #5865f2;
+  color: #fff;
 
   border-radius: 40px;
   font-size: 14px;
   padding: 7px 16px;
   white-space: nowrap;
+
+  &:hover {
+    background-color: hsl(235, 86.1%, 71.8%);
+    box-shadow: 0 8px 15px rgb(0 0 0 / 20%);
+  }
 `;
